2666939e181dfcc470542ad31523869944fa9d70
qyz55 <sjzwf501@126.com>
2017-04-03 22:29:03 +0800

    模板匹配内部实现完成,需要联调

--- src/iarc_recog_track/main.cpp
+++ src/iarc_recog_track/main.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
+#include "ros/ros.h"
+#include "geometry_msgs/Point.h"
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
 #include "highgui.h"
 #include "colotracker.h"
 #include "region.h"
 #include <string>
 
 using namespace cv;
+namespace enc = sensor_msgs::image_encodings; 
 
+cv::Point *PointList;
+cv::Point t_topLeft(0,0);
+cv::Point t_botRight(0,0);
 cv::Point g_topLeft(0,0);
 cv::Point g_botRight(0,0);
 cv::Point g_botRight_tmp(0,0);
+cv::Mat img;
 bool plot = false;
 bool g_trackerInitialized = false;
 ColorTracker * g_tracker = NULL;
@@ -41,11 +50,49 @@ static void onMouse( int event, int x, int y, int, void* param)
     }
 }
 
+void pointCallback1(const geometry_msgs::Point::ConstPtr& msg)
+{
+    ROS_INFO("Hearing geo/Pt_lt:%lf, %lf",msg->x,msg->y);
+    t_topLeft.x = msg->x;
+    t_topLeft.y = msg->y;
+}
+
+void pointCallback2(const geometry_msgs::Point::ConstPtr& msg)
+{
+    ROS_INFO("Hearing geo/Pt_rb:%lf, %lf",msg->x,msg->y);
+    t_botRight.x = msg->x;
+    t_botRight.y = msg->y;
+    g_topLeft = cv::Point(0,0);
+    g_botRight_tmp = cv::Point(0,0);
+    if (g_tracker != NULL)
+        delete g_tracker;
+    g_tracker = new ColorTracker();
+    g_tracker->init(img, t_topLeft.x, t_topLeft.y, t_botRight.x, t_botRight.y);
+    g_trackerInitialized = true;
+}
+
+void imageCallback(const sensor_msgs::ImageConstPtr& original_image) {    
+    cv_bridge::CvImagePtr cv_ptr;    
+    try{
+        cv_ptr = cv_bridge::toCvCopy(original_image, enc::BGR8);    
+    }    
+    catch (cv_bridge::Exception& e) {    
+        ROS_ERROR("tutorialROSOpenCV::main.cpp::cv_bridge exception: %s", e.what());    
+        exit(-1);    
+    } 
+    ROS_INFO("Get Next Picture.");  
+    img = cv_bridge::toCvShare(original_image, "bgr8")->image;
+
+} 
+
 
 int main(int argc, char **argv) 
 {
-    BBox * bb = NULL;
-    cv::Mat img;
+    ros::init(argc, argv, "trackSheepPos");
+    ros::NodeHandle nn;
+    ros::Publisher chatter_pub = nn.advertise<geometry_msgs::Point>("/iarc/sheep/center",20);
+     BBox * bb = NULL;
+    
     int captureDevice = 0;
     if (argc > 1)
         captureDevice = atoi(argv[1]);
@@ -62,7 +109,15 @@ int main(int argc, char **argv)
     cv::namedWindow( "output", 0 );
     cv::setMouseCallback( "output", onMouse, &img);
 
+    ros::Subscriber sub1 = nn.subscribe("geometry_msgs/Point_lefttop", 20, pointCallback1);
+    ros::Subscriber sub2 = nn.subscribe("geometry_msgs/Point_rightbottom", 20, pointCallback2);
+    image_transport::ImageTransport it(nn); 
+    image_transport::Subscriber subI = it.subscribe("sensor_msgs/Image", 1, imageCallback);
+    
+    ros::Rate loop_rate(10);
+   
     for(;;){
+        
 
         webcam >> img; 
 
@@ -92,6 +147,17 @@ int main(int argc, char **argv)
 
         if (bb != NULL){
             cv::rectangle(img, Point2i(bb->x, bb->y), Point2i(bb->x + bb->width, bb->y + bb->height), Scalar(255, 0, 0), 3);
+            if (ros::ok())
+            {
+                geometry_msgs::Point pt;
+                pt.x=bb->x+bb->width/2;
+                pt.y=bb->y+bb->height/2;
+                ROS_INFO("%lf %lf", pt.x,pt.y);
+                chatter_pub.publish(pt);
+                ros::spinOnce();
+                loop_rate.sleep();
+            }
+            
             //cv::rectangle(img, Point2i(bb->x-5, bb->y-5), Point2i(bb->x + bb->width+5, bb->y + bb->height+5), Scalar(0, 0, 255), 1);
             delete bb;
             bb = NULL;
