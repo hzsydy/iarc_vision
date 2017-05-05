bc9f29470965495dc5a1825ed94cef1a98d33143
tianxy14 <tianxy14@mails.tsinghua.edu.cn>
2017-05-05 23:12:58 +0800

    Fsm branch (#6)
    change color input to webcam

---
+++ src/iarc_fsm/src/webcam_capture.cpp
@@ -0,0 +1,42 @@
+#include <opencv2/highgui/highgui.hpp>
+#include "ros/ros.h"
+#include <image_transport/image_transport.h>
+#include <opencv2/core/core.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "webcam");
+    ros::NodeHandle n;
+    image_transport::ImageTransport it(n);
+    image_transport::Publisher pub = it.advertise("/camera/color/image_raw",1);
+
+    VideoCapture capture(1);
+    if (!capture.isOpened())
+    {
+        ROS_INFO("Webcam doesn't open.\n");
+        return 1;
+    }
+    
+    Mat frame;
+    sensor_msgs::ImagePtr msg;
+    
+    ros::Rate loop_rate(50);
+    while(ros::ok())
+    {
+	capture >> frame;
+	if(!frame.empty())
+	{
+	    msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
+	    pub.publish(msg);
+	}
+	ros::spinOnce();
+	loop_rate.sleep();
+    }
+}
+
+
